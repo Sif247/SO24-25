4d6e45a3a8d8420dba1f97260efa15f34228ee6a
Letizia Leonardi <letizia.leonardi@unimore.it>
2025-03-11 15:06:21 +0100

    File comandi per lezione di Mer. 12-03-2025

---
+++ Lezioni/SHELL/LezioneMer12-03-2025/prova1.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo Valore della variabile a = $a
+echo Valore della variabile PATH = $PATH
+echo Valore della variabile HOME = $HOME
+a=hello
+export a
+echo Nuovo valore della variabile a = $a
