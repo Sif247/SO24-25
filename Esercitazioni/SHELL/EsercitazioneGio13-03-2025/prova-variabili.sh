6427c3cf0addecf7b8d9ed7c9aff656b56cec122
letizialeonardi <73306330+letizialeonardi@users.noreply.github.com>
2025-03-17 08:35:25 +0100

    Fle cancellati per errore

--- Esercitazioni/SHELL/EsercitazioneGio13-03-2025/prova-variabili.sh
+++ Esercitazioni/SHELL/EsercitazioneGio13-03-2025/prova-variabili.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#File comandi prova-variabili.sh
+#File prova-variabili.sh
 echo Il valore della variabile x = $x #stampo il valore della variabile x
 echo Il valore della variabile y = $y #stampo il valore della variabile y
 echo Il valore della variabile z = $z #stampo il valore della variabile z
